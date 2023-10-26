b08fd2dbc0738fddb2181db90854c804ad5ed091
KaraitPL <karaitpl@gmail.com>
2023-10-26 07:22:17 +0200

    Initial commit

---
+++ src/main/java/com/example/SongLibraryApp/SongLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SongLibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongLibraryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SongLibraryApp/SongLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SongLibraryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongLibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
