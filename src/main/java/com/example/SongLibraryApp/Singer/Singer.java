b60d2a57762e2b64a83b55f70b4f689426ce7a75
KaraitPL <karaitpl@gmail.com>
2023-10-26 08:07:54 +0200

    Singer and Song class created

---
+++ src/main/java/com/example/SongLibraryApp/Singer/Singer.java
@@ -0,0 +1,88 @@
+package com.example.SongLibraryApp.Singer;
+
+import com.example.SongLibraryApp.Song.Song;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "singers")
+public class Singer implements Comparable<Singer>, Serializable {
+
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(mappedBy = "singer")
+    @Builder.Default
+    private List<Song> songs = new ArrayList<>();
+
+
+
+
+    @Override
+    public String toString() {
+        return "\nSinger{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", songs=" + songs +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Singer singer = (Singer) o;
+        return age == singer.age && Objects.equals(name, singer.name) && Objects.equals(songs, singer.songs);
+    }
+
+    @Override
+    public int hashCode() {
+        int sum = 0;
+        for (Song song : songs) {
+            sum += song.getName().length();
+            sum += song.getYear();
+        }
+        return name.length() + age + sum;
+    }
+
+    @Override
+    public int compareTo(Singer other){
+        if(this.name.compareTo(other.name) != 0){
+            return this.name.compareTo(other.name);
+        }
+        else if(this.age != other.age){
+            return this.age - other.age;
+        }
+        else{
+            return this.songs.size() - other.songs.size();
+        }
+    }
+
+    public void addSong(Song song){
+        songs.add(song);
+    }
+
+    public void sortSongs(){
+        songs.sort(Song::compareTo);
+    }
+}
---
+++ src/main/java/com/example/SongLibraryApp/Song/Song.java
@@ -0,0 +1,77 @@
+package com.example.SongLibraryApp.Song;
+
+import com.example.SongLibraryApp.Singer.Singer;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "songs")
+public class Song implements Comparable<Song>, Serializable {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year")
+    private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "singer")
+    private Singer singer;
+
+    public static class SongBuilder {
+        public Song build() {
+            UUID id = new UUID(3,4); //TODO Change how UUID is created
+            Song song = new Song(id, this.name, this.year, this.singer);
+            singer.addSong(song);
+            return song;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                ", singer=" + singer.getName() +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return year == song.year && Objects.equals(name, song.name) && Objects.equals(singer, song.singer);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.length() + year + singer.getName().length();
+    }
+
+    @Override
+    public int compareTo(Song other){
+        if(this.name.compareTo(other.name) != 0){
+            return this.name.compareTo(other.name);
+        }
+        else if(this.year != other.year){
+            return this.year - other.year;
+        }
+        else{
+            return this.singer.compareTo(other.singer);
+        }
+    }
+
+}
